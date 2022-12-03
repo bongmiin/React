d8bd8b2e88c4b2d19d82d8c490521b6c3a14afcf
bongmiin <102722487+bongmiin@users.noreply.github.com>
2022-12-03 19:19:40 +0900

    commit 12-03

--- todo_app/src/App.js
+++ todo_app/src/App.js
@@ -1,44 +1,26 @@
-import { useState } from 'react';
-import Calendar from 'react-calendar';
-import './App.css';
-import './Calendar.css';
+import React from 'react';
+import { createGlobalStyle } from 'styled-components';
+import TodoTemplate from './components/TodoTemplate';
+import TodoHead from './components/TodoHead';
+import TodoList from './components/TodoList';
+import TodoCreate from './components/TodoCreate';
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    background: #e9ecef;
+  }
+`;
 
 function App() {
-  const [date, setDate] = useState(new Date());
-  const [todos, setTodos] = useState(["대", "한", "민", "국"]);
-
-  let clickHandler = () =>{
-    let today = new Date();
-  }
-
-  const test = () =>{
-    
-  }
   return (
-    <div className='app'>
-      <h1 className='text-center'>React Calendar</h1>
-      <div className='calendar-container'>
-        <Calendar onChange={test} value={date} />
-      </div>
-      <button onClick={clickHandler}>click me!</button>
-      <br/>
-      {
-      todos.map((char)=>{
-        if(char !== "한"){
-
-          return(
-            <p>
-              {char}
-            </p>
-          )
-        }
-      })
-      }
-      <p className='text-center'>
-        <span className='bold'>Selected Date:</span>{' '}
-        {date.toDateString()}
-      </p>
-    </div>
+    <>
+      <GlobalStyle />
+      <TodoTemplate>
+        <TodoHead />
+        <TodoList />
+        <TodoCreate />
+      </TodoTemplate>
+    </>
   );
 }
 
